e50b9e47694114a8623505ffa8ebd227b380721c
Anjaya <anjayainduwara@gmail.com>
2024-08-13 08:51:18 +0530

    first commit

--- lib/widgets/summary.dart
+++ lib/widgets/summary.dart
@@ -1,4 +1,5 @@
 import 'package:fitnessapp/constants/colors.dart';
+import 'package:fitnessapp/utils/responsive.dart';
 import 'package:fitnessapp/widgets/pie_chart.dart';
 import 'package:fitnessapp/widgets/scheduled_widget.dart';
 import 'package:fitnessapp/widgets/summary_details.dart';
@@ -9,26 +10,32 @@ class Summary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(20),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            PieChartCard(),
-            Text(
-              "Summary",
-              style: TextStyle(
-                  fontSize: 16, fontWeight: FontWeight.w600, color: greyColor),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            SummaryDetails(),
-            SizedBox(
-              height: 15,
-            ),
-            ScheduledWidget(),
-          ],
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Container(
+      decoration: !isDesktop ? BoxDecoration(color: cardBgColor) : null,
+      child: const Padding(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              PieChartCard(),
+              Text(
+                "Summary",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: greyColor),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SummaryDetails(),
+              SizedBox(
+                height: 15,
+              ),
+              ScheduledWidget(),
+            ],
+          ),
         ),
       ),
     );
